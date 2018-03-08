13dbb19ef871951fd4a4ff8c78a8e84b4f613166
Jacob Edmond <jacobedmond@jacobs-macbook-pro.local>
2018-03-08 06:48:38 -0600

    Added logic for determining when two cards are flipped and then seeing if they match or not.  If they match, the cards will be removed.  If they do not match, they will flip back over.

--- MatchApp/ViewController.swift
+++ MatchApp/ViewController.swift
@@ -15,6 +15,8 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     var model = CardModel()
     var cardArray = [Card]()
     
+    var firstFlippedCardIndex:IndexPath?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -40,16 +42,101 @@ class ViewController: UIViewController, UICollectionViewDelegate, UICollectionVi
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        // Get a CardCollectionViewCell object
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath) as! CardCollectionViewCell
+        
+        // Get the card that the collection view is trying to display
+        let card = cardArray[indexPath.row]
+        
+        // Set that card for the cell
+        cell.setCard(card)
         
         return cell
     }
 
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         
+        // Get the cell the user selected
+        let cell = collectionView.cellForItem(at: indexPath) as! CardCollectionViewCell
         
+        // Get the card the user selected
+        let card = cardArray[indexPath.row]
+        
+        if card.isFlipped == false && card.isMatched == false {
+            
+            // Flip the card
+            cell.flip()
+            
+            // Set the status of the card
+            card.isFlipped = true
+            
+            // Determine if it's the first card or second card that's flipped over
+            if firstFlippedCardIndex == nil {
+                
+                // This is the first card being flipped
+                firstFlippedCardIndex = indexPath
+            }
+            else {
+                // This is the second card being flipped
+                
+                // Perform the matching logic
+                checkForMatches(indexPath)
+            }
+            
+        }
+        
+
+    } // End the didSelectItemAt method
+    
+    // MARK: - Game Logic Methods
+    
+    func checkForMatches(_ secondFlippedCardIndex:IndexPath) {
+        
+        // Get the cells for the two cards that were revealed
+        let cardOneCell = collectionView.cellForItem(at: firstFlippedCardIndex!) as? CardCollectionViewCell
+        
+        let cardTwoCell = collectionView.cellForItem(at: secondFlippedCardIndex) as? CardCollectionViewCell
+        
+        // Get the cards for the two cards that were revealed
+        let cardOne = cardArray[firstFlippedCardIndex!.row]
+        let cardTwo = cardArray[secondFlippedCardIndex.row]
+        
+        // Compare the two cards
+        if cardOne.imageName == cardTwo.imageName {
+            
+            // It's a match
+            
+            // Set the statuses of the cards
+            cardOne.isMatched = true
+            cardTwo.isMatched = true
+            
+            // Remoe the cards from the grid
+            cardOneCell?.remove()
+            cardTwoCell?.remove()
+        }
+        else {
+            
+            // It's not a match
+            
+            // Set the statuses of the cards
+            cardOne.isFlipped = false
+            cardTwo.isFlipped = false
+            
+            // Flip both cards back
+            cardOneCell?.flipBack()
+            cardTwoCell?.flipBack()
+        }
+        
+        // Tell the collectionView to reload the cell of hte first card if it is nil
+        if cardOneCell == nil {
+            collectionView.reloadItems(at: [firstFlippedCardIndex!])
+        }
+        
+        // Reset the property that tracks that first card flipped
+        firstFlippedCardIndex = nil
     }
-}
+    
+} // End ViewController Class
 
 
 
